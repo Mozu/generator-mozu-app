8bb47f0019b9eaf5825a560c581a7884e34728a8
james <zetlen@gmail.com>
2015-05-18 18:53:17 -0500

    adding more generic functionality

--- generators/app/index.js
+++ generators/app/index.js
@@ -16,21 +16,18 @@ try {
 
 module.exports = yeoman.generators.Base.extend({
 
-  _intro: 'Follow the prompts to scaffold a Mozu App package. The resulting directory will include package metadata and a sync tool.',
-
-   // note: arguments and options should be defined in the constructor.
   constructor: function () {
     yeoman.generators.Base.apply(this, arguments);
-
-    this.helpers = Object.keys(helpers).reduce(function(obj, name) {
-      obj[name] = helpers[name].bind(this);
-      return obj;
-    }.bind(this), {});
 
     // This option adds support for non-production environments
     this.option('internal', {
       type: 'Boolean',
       defaults: false,
+      hide: true
+    });
+
+    this.option('intro', {
+      defaults: 'Follow the prompts to scaffold a Mozu App package. The resulting directory will include package metadata and a sync tool.',
       hide: true
     });
 
@@ -63,214 +60,215 @@ module.exports = yeoman.generators.Base.extend({
     }
   },
 
-  acquireGitStatus: function() {
-    var done = this.async();
-    quickGitHits.detectDirectory(this.destinationPath(), function(err, result) {
-      if (err) {
-        throw err;
-      }
-      helpers.addAsPrivateProps(this, result);
-      done();
-    }.bind(this));
-  },
-
-  displayMozuBanner: function(message) {
-    if (!message) message = this._intro;
-    if (this.options['skip-prompts']) {
-      if (!this.config.get('domains')) {
-        message = 'You cannot skip prompts if you have never run this generator in the current directory! Run again without the --skip-prompts or --quick options.';
-        this.log(mosay(message));
-        throw new Error(message);
-      }
-      message += '\n\nSkipping prompts step because --skip-prompts was specified. Rerunning...';
-    }
-
-    this.log(mosay(message));
-  },
-
-
-  promptForEnvironment: function(cb) {
-
-    var self = this;
-    if (!cb) cb = this.async();
-
-    function done() {
-      self.developerInfoKeys = [
-        'AccountLogin',
-        'AppKey',
-        'SharedSecret'
-      ].reduce(function(memo, keyName) {
-        memo[keyName] = self._mozuEnv.name + '_' + keyName;
-        return memo;
-      }, {});
-      cb();
-    }
-
-    if (this.options.internal) {
-
-      helpers.promptAndSaveResponse(self, [{
-        type: 'list',
-        name: 'mozuEnv',
-        message: 'Select Mozu environment:',
-        default: this.config.get('homePod') || PROD_HOMEPOD,
-        choices: Object.keys(XDMetadata.environments).map(function(envName) {
-          return {
-            name: envName,
-            value: {
-              name: envName,
-              domain: XDMetadata.environments[envName].homeDomain
-            }
-          };
-        })
-      }], done);
-
-    } else {
-
-      if (!PROD_HOMEPOD) {
-        throw new Error("Metadata package does not contain a home pod URL for `Production/Sandbox` environment.");
-      }
-
-      this._mozuEnv = {
-        name: PROD_NAME,
-        domain: PROD_HOMEPOD
-      };
-
-      done();
-
-    }
-
-  },
-
-  promptForDeveloperAccount: function(cb) {
-
-    var self = this;
-    var done = cb || this.async();
-
-    var prompts = [{
-      type: 'input',
-      name: this.developerInfoKeys.AccountLogin,
-      message: 'Enter your Developer Account login email:',
-      filter: helpers.trimString,
-      store: true
-    }, {
-      type: 'input',
-      name: this.developerInfoKeys.AppKey,
-      message: 'Application Key for your developer sync app:',
-      filter: helpers.trimString,
-      store: true
-    }, { 
-      type: 'password',
-      name: this.developerInfoKeys.SharedSecret,
-      message: 'Shared Secret for your developer sync app:',
-      filter: helpers.trimString,
-      store: true
-    }];
-
-    helpers.promptAndSaveResponse(self, prompts, function getDeveloperAccountId() {
-      var developerAccountId = self.config.get('developerAccountId');
-      if (developerAccountId) {
-        self._developerAccountId = developerAccountId;
-        done();
-      } else {
-        helpers.promptAndSaveResponse(self, [{
-          type: 'password',
-          name: 'password', 
-          message: 'Developer Account password:',
-          validate: function(str) {
-            return !!str;
-          }
-        }], function() {
-          helpers.remark(self, 'Looking up developer accounts...');
-          var context = helpers.makeSDKContext(self);
-          context.developerAccount.password = self._password;
-          var p = SDK.client(context)
-            .platform()
-            .developer()
-            .developerAdminUserAuthTicket()
-            .createDeveloperUserAuthTicket(context.developerAccount).then(function(res) {
-              if (!res.availableAccounts) {
-                helpers.lament(self, "No available accounts found for " + context.developerAccount.emailAddress);
-              } else {
-                var accountChoices = res.availableAccounts.map(function(acct) {
-                  return {
-                    name: acct.name + ' (' + acct.id + ')',
-                    value: acct.id
-                  };
-                });
-
-                helpers.promptAndSaveResponse(self, [{
-                  type: 'list',
-                  name: 'developerAccountId',
-                  choices: accountChoices,
-                  message: 'Select a developer account for ' + chalk.bold.cyan(self._developerAccountLogin) + ':',
-                  default: self.config.get('developerAccountId')
-                }], done);
-              }
-            }, function(err) {
-              if (err.originalError && err.originalError.errorCode === "INVALID_CREDENTIALS") {
-                // terrible, awful, but can't figure out the lifecycle here and when.js is logging
-                // a potential pending rejection which messes up the prompt
-                SDK.suppressUnhandledRejections();
-                helpers.lament(self, 'Invalid credentials. Retry password (or Ctrl-C to quit).')
-                return getDeveloperAccountId();
-              } else {
-                helpers.lament(self, err.message);
-                process.exit(1);
-              }
-            });
-        });
-      }
-      return true;
-    });
-
-  },
-
-  promptForGitRepo: function(done) {
-
-    done = done || this.async();
-
-    helpers.promptAndSaveResponse(this, [{
-      type: 'confirm',
-      name: 'createGit',
-      message: 'Create Git repository?',
-      filter: helpers.trimString,
-      when: (function() {
-        return this._gitInstalled && !this._inGitRepo && this.config.get('createGit') !== false;
-      }.bind(this))
-    }], done);
-
-  },
-
-  saveMozuConfig: function(extras) {
-    if (!this.options['skip-prompts']) {
-      this.fs.writeJSON(
-        this.destinationPath('mozu.config.json'),
-        helpers.merge(helpers.makeSDKContext(this), extras || {})
-      );
-      this.config.set('homePod', this._mozuEnv.domain || PROD_HOMEPOD);
-      this.config.set('applicationKey', this._applicationKey);
-      this.config.set('developerAccountId', this._developerAccountId);
-      this.config.set('createGit', this._createGit);
-    }
-  },
-
-  createRepoIfRequested: function(done) {
-    if (!done) done = this.async();
-    console.log('got here');
-    if (this._createGit) {
-      quickGitHits.createRepoInDirectory(this.destinationPath(), { repositoryUrl: this._repositoryUrl }, function(err) {
+  initializing: {
+    acquireGitStatus: function() {
+      var done = this.async();
+      quickGitHits.detectDirectory(this.destinationPath(), function(err, result) {
         if (err) {
           throw err;
         }
-        helpers.remark(this, 'Created git repository');
-        if (this._repositoryUrl) {
-          helpers.remark(this, 'Added remote ' + this._repositoryUrl + ' to git repository');
-        }
+        helpers.addAsPrivateProps(this, result);
         done();
       }.bind(this));
-    } else {
-      done();
+    },
+    displayMozuBanner: function(message) {
+      if (!message) message = this.options.intro;
+      if (this.options['skip-prompts']) {
+        if (!this.fs.exists('mozu.config.json')) {
+          message = 'You cannot skip prompts if you have never run this generator in the current directory! Run again without the --skip-prompts or --quick options.';
+          this.log(mosay(message));
+          throw new Error(message);
+        }
+        message += '\n\nSkipping prompts step because --skip-prompts was specified. Rerunning...';
+      }
+
+      this.log(mosay(message));
     }
+  },
+
+  prompting: {
+
+    promptForEnvironment: function(cb) {
+
+      var self = this;
+      if (!cb) cb = this.async();
+
+      function done() {
+        self._homePod = XDMetadata.environments[self._mozuEnv].homeDomain;
+        self.developerInfoKeys = [
+          'AccountLogin',
+          'AppKey',
+          'SharedSecret'
+        ].reduce(function(memo, keyName) {
+          memo[keyName] = self._mozuEnv + '_' + keyName;
+          return memo;
+        }, {});
+        cb();
+      }
+
+      if (this.options.internal) {
+
+        helpers.promptAndSaveResponse(self, [{
+          type: 'list',
+          name: 'mozuEnv',
+          message: 'Select Mozu environment:',
+          default: this.config.get('mozuEnv') || PROD_NAME,
+          choices: Object.keys(XDMetadata.environments)
+        }], done);
+
+      } else {
+        if (!PROD_HOMEPOD) {
+          throw new Error("Metadata package does not contain a home pod URL for `Production/Sandbox` environment.");
+        }
+        this._mozuEnv = PROD_NAME;
+        done();
+      }
+
+    },
+
+    promptForDeveloperAccount: function(cb) {
+
+      var self = this;
+      var done = cb || this.async();
+
+      var prompts = [{
+        type: 'input',
+        name: this.developerInfoKeys.AccountLogin,
+        message: 'Enter your Developer Account login email:',
+        filter: helpers.trimString,
+        store: true
+      }, {
+        type: 'input',
+        name: this.developerInfoKeys.AppKey,
+        message: 'Application Key for your developer sync app:',
+        filter: helpers.trimString,
+        store: true
+      }, { 
+        type: 'password',
+        name: this.developerInfoKeys.SharedSecret,
+        message: 'Shared Secret for your developer sync app:',
+        filter: helpers.trimString,
+        store: true
+      }];
+
+      helpers.promptAndSaveResponse(self, prompts, function getDeveloperAccountId() {
+        var developerAccountId = self.config.get('developerAccountId');
+        if (developerAccountId) {
+          self._developerAccountId = developerAccountId;
+          done();
+        } else {
+          helpers.promptAndSaveResponse(self, [{
+            type: 'password',
+            name: 'password', 
+            message: 'Developer Account password:',
+            validate: function(str) {
+              return !!str;
+            }
+          }], function() {
+            helpers.remark(self, 'Looking up developer accounts...');
+            var context = helpers.makeSDKContext(self);
+            context.developerAccount.password = self._password;
+            var p = SDK.client(context)
+              .platform()
+              .developer()
+              .developerAdminUserAuthTicket()
+              .createDeveloperUserAuthTicket(context.developerAccount).then(function(res) {
+                if (!res.availableAccounts) {
+                  helpers.lament(self, "No available accounts found for " + context.developerAccount.emailAddress);
+                } else {
+                  var accountChoices = res.availableAccounts.map(function(acct) {
+                    return {
+                      name: acct.name + ' (' + acct.id + ')',
+                      value: acct.id
+                    };
+                  });
+
+                  helpers.promptAndSaveResponse(self, [{
+                    type: 'list',
+                    name: 'developerAccountId',
+                    choices: accountChoices,
+                    message: 'Select a developer account for ' + chalk.bold.cyan(self['_' + self.developerInfoKeys.AccountLogin]) + ':',
+                    default: self.config.get('developerAccountId')
+                  }], done);
+                }
+              }, function(err) {
+                if (err.originalError && err.originalError.errorCode === "INVALID_CREDENTIALS") {
+                  // terrible, awful, but can't figure out the lifecycle here and when.js is logging
+                  // a potential pending rejection which messes up the prompt
+                  SDK.suppressUnhandledRejections();
+                  helpers.lament(self, 'Invalid credentials. Retry password (or Ctrl-C to quit).')
+                  return getDeveloperAccountId();
+                } else {
+                  helpers.lament(self, err.message);
+                  process.exit(1);
+                }
+              });
+          });
+        }
+        return true;
+      });
+
+    },
+
+    promptForGitRepo: function(done) {
+
+      done = done || this.async();
+
+      if (this._gitInstalled && !this._inGitRepo && this.config.get('createGit') !== false) {
+
+        helpers.promptAndSaveResponse(this, [{
+          type: 'confirm',
+          name: 'createGit',
+          message: 'Create Git repository?',
+          filter: helpers.trimString
+        }], done);
+
+      } else {
+        done();
+      } 
+
+    }
+  },
+
+  configuring: {
+
+    saveMozuConfig: function() {
+      if (!this.options['skip-prompts']) {
+        this.fs.writeJSON(
+          this.destinationPath('mozu.config.json'),
+          helpers.makeSDKContext(this)
+        );
+        this.config.set('mozuEnv', this._mozuEnv);
+        this.config.set('developerAccountId', this._developerAccountId);
+        this.config.set('createGit', this._createGit);
+      }
+    }
+
+  },
+
+  installing: {
+
+    createRepoIfRequested: function(done) {
+      if (!done) done = this.async();
+      console.log('got here');
+      if (this._createGit) {
+        quickGitHits.createRepoInDirectory(this.destinationPath(), { repositoryUrl: this._repositoryUrl }, function(err) {
+          if (err) {
+            throw err;
+          }
+          helpers.remark(this, 'Created git repository');
+          if (this._repositoryUrl) {
+            helpers.remark(this, 'Added remote ' + this._repositoryUrl + ' to git repository');
+          }
+          done();
+        }.bind(this));
+      } else {
+        done();
+      }
+    }
+
   }
 
+}, {
+  helpers: helpers
 });
